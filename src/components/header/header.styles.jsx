25e773f2006008838476bfd1bf0b432a923f8b8f
Cassandra Rossall <cassandrarossall@gmail.com>
2021-02-15 22:04:08 -0500

    enhanced background image quality, fixed contact btn animation

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,89 +1,87 @@
-import styled, {css} from 'styled-components'
-import {Link} from 'gatsby'
-import colors from '../../config/colors'
+import styled, { css } from "styled-components"
+import { Link } from "gatsby"
+import colors from "../../config/colors"
 
 export const HeaderContainer = styled.header`
-box-shadow: 0px 2px 6px rgba(18, 18, 18, .1);
-background: #fff;
-transition: transform .3s ease, background 0s ease 0s;
-position: sticky;
--webkit-transform: ${({sticky}) => sticky ? `translateY(0px)` : `translateY(-300px)`};
--ms-transform: ${({sticky}) => sticky ? `translateY(0px)` : `translateY(-300px)`};
-transform: ${({sticky}) => sticky ? `translateY(0px)` : `translateY(-300px)`};
-top: 0;
-z-index: 100;
-padding: 5px 40px;
+  box-shadow: 0px 2px 6px rgba(18, 18, 18, 0.1);
+  background: #fff;
+  transition: transform 0.3s ease, background 0s ease 0s;
+  position: sticky;
+  -webkit-transform: ${({ sticky }) =>
+    sticky ? `translateY(0px)` : `translateY(-300px)`};
+  -ms-transform: ${({ sticky }) =>
+    sticky ? `translateY(0px)` : `translateY(-300px)`};
+  transform: ${({ sticky }) =>
+    sticky ? `translateY(0px)` : `translateY(-300px)`};
+  top: 0;
+  z-index: 100;
+  padding: 5px 40px;
 
-    .cta {
-        animation: .6s fly ease;
-    }
-
-    @media(max-width: 960px) {
-        padding: 20px;
-        box-shadow: none;
-
-        #ctaBtn {
-            display: none;
-        }
-    }
-
-
-${({isOpen}) => isOpen && css`
+  @media (max-width: 960px) {
+    padding: 20px;
     box-shadow: none;
-`}
+
+    #ctaBtn {
+      display: none;
+    }
+  }
+
+  ${({ isOpen }) =>
+    isOpen &&
+    css`
+      box-shadow: none;
+    `}
 `
 
 export const Navbar = styled.nav`
-margin: 0 auto;
-padding: 0;
-display: -webkit-box; 
-display: -ms-flexbox;
-display: -webkit-flex;
-display: flex;  
-justify-content: space-between;
-align-items: center
+  margin: 0 auto;
+  padding: 0;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 export const Logo = styled(Link)`
-color: ${colors.dark};
-text-decoration: none;
-line-height: 28px;
+  color: ${colors.dark};
+  text-decoration: none;
+  line-height: 28px;
 
-    h2 {
-        font-weight: 800;
-        font-size: 26px;
+  h2 {
+    font-weight: 800;
+    font-size: 26px;
 
-        span {
-            font-weight: 400;
-            font-size: 19px;
-            padding-left: 5px;
+    span {
+      font-weight: 400;
+      font-size: 19px;
+      padding-left: 5px;
 
-            @media(max-width: 900px) {
-                display: block;
-                padding-left: 0;
-                padding-top: 5px;
-            }
+      @media (max-width: 900px) {
+        display: block;
+        padding-left: 0;
+        padding-top: 5px;
+      }
 
-            @media(max-width: 500px) {
-                font-size: 16px;
-            }
-        }
-
-        @media(max-width: 700px) {
-            font-size: 22px;
-
-            span {
-                font-size: 17px;
-            }
-        }
+      @media (max-width: 500px) {
+        font-size: 16px;
+      }
     }
 
-// ${({isOpen}) => isOpen && css`
-//     opacity: 0;
-// `}
+    @media (max-width: 700px) {
+      font-size: 22px;
+
+      span {
+        font-size: 17px;
+      }
+    }
+  }
 `
 
 export const Arrow = styled.span`
-margin-left: 10px;
-transition: all .7s ease;
-transition-delay: .2s;
+  margin-left: 10px;
+
+  &.cta {
+    animation: 0.5s fly ease;
+  }
 `
--- src/components/title/title.styles.jsx
+++ src/components/title/title.styles.jsx
@@ -1,25 +1,27 @@
-import styled, {css} from 'styled-components'
-import colors from '../../config/colors'
+import styled, { css } from "styled-components"
+import colors from "../../config/colors"
 
 export const TitleContainer = styled.h2`
-color: ${colors.secondary};
-font-size: ${({isLarge}) => isLarge ? '4.5rem' : '2.5rem'};
--webkit-text-stroke: 1px ${colors.dark};
-position: relative;
-font-weight: 900;
-margin-bottom: 20px;
-margin-top: ${({isLarge}) => !isLarge && '35px'};
-line-height: 100%;
+  color: ${colors.secondary};
+  font-size: ${({ isLarge }) => (isLarge ? "4.5rem" : "2.5rem")};
+  -webkit-text-stroke: 1px ${colors.dark};
+  position: relative;
+  font-weight: 900;
+  margin-bottom: 20px;
+  margin-top: ${({ isLarge }) => !isLarge && "35px"};
+  line-height: 100%;
 
-${({isPurple}) => isPurple && css`
-    color: ${colors.primary};
-`}
+  ${({ isPurple }) =>
+    isPurple &&
+    css`
+      color: ${colors.primary};
+    `}
 
-span {
+  span {
     color: ${colors.accent};
-}
+  }
 
-@media(max-width: 750px) {
-    font-size: ${({isLarge}) => isLarge ? '3rem' : '2rem'};
-}
+  @media (max-width: 750px) {
+    font-size: ${({ isLarge }) => (isLarge ? "3rem" : "2rem")};
+  }
 `
